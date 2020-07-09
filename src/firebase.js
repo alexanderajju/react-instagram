37cec75d141d38cc5b0e6f5cd30cca37e07c5875
alexanderajju <alexanderajju@gmail.com>
2020-07-09 09:43:06 +0530

    post functional

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKoSrk01iwhkHvP8-QJ6gOlSncCF0O2T0",
+    authDomain: "social-clone-67b59.firebaseapp.com",
+    databaseURL: "https://social-clone-67b59.firebaseio.com",
+    projectId: "social-clone-67b59",
+    storageBucket: "social-clone-67b59.appspot.com",
+    messagingSenderId: "353967823647",
+    appId: "1:353967823647:web:06f716e427c1fd19a22e2a",
+    measurementId: "G-XM0FNMEJPJ"
+  };
