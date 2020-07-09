c17a56feebacc471d0b1f87e1b07e4fe34631fde
alexanderajju <alexanderajju@gmail.com>
2020-07-09 13:16:32 +0530

    SignUp and Logout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,158 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
-import {db } from './firebase'
+import { db , auth} from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50; 
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
   const [posts, setPosts] = useState([
-    {}
     
   ]);
+  const [open , setOpen] = useState(false);
+  const [username , setUsername] = useState('');
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState('');
+  const [user,setUser] = useState('')
+
+  useEffect(()=>{
+const unsubscribe= auth.onAuthStateChanged((authUser) => {
+  if (authUser){
+    console.log(authUser);
+    setUser(authUser);
+
+    if (authUser.displayName){
+
+    }else{
+      return authUser.updateProfile({
+        displayName: username
+      });
+    }
+  }else{
+    setUser(null);
+
+  }
+})
+return () =>{
+  unsubscribe();
+}
+  },[user, username]);
+
+
+
 
   useEffect(() =>{
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc =>({
-        id : doc.id,
-        post : doc.data()})))
+      setPosts(snapshot.docs.map(doc => ({
+        id:doc.id,
+        post:doc.data()
+      })));
     });
 
   }, []);
 
- 
+
+  
+
+ const signUp = (event) =>{
+   event.preventDefault();
+   auth
+   .createUserWithEmailAndPassword(email,password)
+   .then((authUser) => {
+     return authUser.user.updateProfile({
+       displayName:username
+     })
+   })
+   .catch((error) => alert(error.message))
+
+    
+    
+    
+ }
 
 
   return (
+    
+
+
+
     <div className="App">
+     <Modal
+        open={open}
+        onClose={()=> setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+
+  <form className="app__signup">
+      <img 
+        className="app__signup__logo"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt="instagram"
+          />
+      <Input
+        placeholder="username"
+        type="text"
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        type="text"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+      />
+        <Button type="submit" onClick={signUp}>Sign Up</Button>
+      
+  
+  </form>
+        
+     
+      </div>
+      </Modal>
+    
+
       <div className="app__header" >
       <img 
       className="app__headerImage" 
@@ -31,9 +161,19 @@ function App() {
       />
       </div>
 
+      {user ? (
+        <Button onClick={() => auth.signOut()}>LogOut</Button>
+
+      ):(
+        <Button onClick={() => setOpen(true)}>SignUp</Button>
+
+      )}
+      
+
+
     {
-      posts.map(({id ,post}) => (
-        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+      posts.map(({id,post}) => (
+        <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
       ))
     }
       
--- src/firebase.js
+++ src/firebase.js
@@ -16,4 +16,4 @@ const db =firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
-export {db, auth, storage};
+export { db, auth, storage };
